bf1738a3fd0a13aaa3c916014e3a826fcaef54b3
IvanH <ivanhernandez02@aragon.unam.mx>
2022-03-16 20:49:32 -0600

    se creo el proyecto la clase main

---
+++ Matriz/src/main/java/main/Main.java
@@ -0,0 +1,9 @@
+package main;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("BIENVENIDOS AL PROGRAMA QUE ESCALONA MATRICES");
+        System.out.println("INTRODUCE EL NUMERO DE ECUACIONES Y EL NUMERO DE VARIABLES QUE REQUIERES");
+    }
+   
+}
